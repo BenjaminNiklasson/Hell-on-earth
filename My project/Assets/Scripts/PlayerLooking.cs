bacf087c49caee822e690b2676f6ac4f2154277e
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-02-03 10:54:07 +0100

    yipeee

--- My project/Assets/Scripts/PlayerLooking.cs
+++ My project/Assets/Scripts/PlayerLooking.cs
@@ -5,12 +5,6 @@ using UnityEngine.UIElements;
 
 public class PlayerLooking : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
