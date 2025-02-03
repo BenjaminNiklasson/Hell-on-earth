33c2aef53a6da38c623214a03c139ad5251a862f
BusterNoren08 <buster.noren@elev.ga.lbs.se>
2025-02-03 11:30:00 +0100

    health player

---
+++ My project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int playerHealth = 1;
+    [SerializeField] float invincibilityTime = 2f;
+    bool invincible = false;
+    void DisableInvincibility()
+    {
+        invincible = false;
+    }
+        
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Hazard"))
+        {
+            if (invincible == true)
+            {
+                return;
+            }
+            if (playerHealth > 0)
+            {
+                playerHealth--;
+                invincible = true;
+                Invoke("DisableInvincibility", invincibilityTime);
+                Debug.Log("Player health:" + playerHealth);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+              
+                
+           
+           
+            
+        }
+    }
+
+    
+}
