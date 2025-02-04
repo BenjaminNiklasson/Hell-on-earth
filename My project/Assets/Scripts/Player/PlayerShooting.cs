56360fcbd9a14e1dad79eaef85ed83c611d5474e
NilasAlbinson <nilas.albinson@gmail.com>
2025-02-04 16:12:46 +0100

    Panik, min dator dör snabbt save
    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- My project/Assets/Scripts/Player/PlayerShooting.cs
+++ My project/Assets/Scripts/Player/PlayerShooting.cs
@@ -7,41 +7,119 @@ using UnityEngine.InputSystem;
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] float playerBulletSpeed = 10f;
-    [SerializeField] GameObject PlayerBullet;
-    [SerializeField] GameObject PlayerGun;
-    [SerializeField] int ammoMax = 20;
+    [SerializeField] GameObject playerBullet;
+    [SerializeField] GameObject playerGun;
+    [SerializeField] int pistolAmmoMax = 20;
+    [SerializeField] int shotgunAmmoMax = 3;
+    [SerializeField] int minigunAmmoMax = 100;
     [SerializeField] float reloadTime = 5;
+    [SerializeField] int numShotgunShoots;
+    [SerializeField] float shotgunSpreadDegrees;
+    [SerializeField] float shotgunColdownTime;
     bool isReloading = false;
+    bool shotgunColdown = false;
     int ammo;
+    bool playerHasShotgun = false;
+    bool playerHasMinigun = true;
+    bool playerHasPistol = false;
 
     private void Start()
     {
-        ammo = ammoMax;
+        ResetAmmo();
     }
 
     void OnFire()
     {
-        if (ammo < 1)
+        if (playerHasShotgun)
         {
-            ammo = ammoMax;
-            Invoke("ReloadDone", reloadTime);
-            isReloading = true;
-            //If our ammo is 0 or less we start reloading, reset our ammo and makes sure we will stop reloading after reloadTime seconds.
+            if (ammo < 1)
+            {
+                ResetAmmo();
+                Invoke("ReloadDone", reloadTime);
+                isReloading = true;
+                //If our ammo is 0 or less we start reloading, reset our ammo and makes sure we will stop reloading after reloadTime seconds.
+            }
+            if (isReloading)
+            {
+                return;
+            }
+            else if (shotgunColdown)
+            {
+                return;
+            }
+            else
+            {
+                Debug.Log(numShotgunShoots);
+                for (int i = 0; i < numShotgunShoots; i++)
+                {
+                    Debug.Log("Pang!");
+                    float baseRotationZ = playerGun.transform.rotation.eulerAngles.z;
+                    float randomOffset = Random.Range(-shotgunSpreadDegrees, shotgunSpreadDegrees);
+                    Quaternion bulletSpawnRotation = Quaternion.Euler(0, 0, baseRotationZ + randomOffset);
+
+                    GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, bulletSpawnRotation);
+                    Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                    rb.AddForce(bullet.transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+                }
+                ammo -= 1;
+                Debug.Log($"The shotgun has {ammo} ammo!");
+                shotgunColdown = true;
+                Invoke("ShotgunColdownDone", shotgunColdownTime);
+            }
         }
-        if (isReloading)
+        else if (playerHasPistol)
         {
-            return;
+            if (ammo < 1)
+            {
+                ResetAmmo();
+                Invoke("ReloadDone", reloadTime);
+                isReloading = true;
+                //If our ammo is 0 or less we start reloading, reset our ammo and makes sure we will stop reloading after reloadTime seconds.
+            }
+            if (isReloading)
+            {
+                return;
+            }
+            else
+            {
+                GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
+                //We create a bullet at the guns position and rotation as well as saving the information about that bullet in a gameobject variable.
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                //We grab the rigidbodycomponent from the saved bullet and save it in the variable "rb".
+                rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+                ammo -= 1;
+                //Adds force to the rb of the bullet and increases it acording to bulletSpeed. Also decreses ammo by one.
+                Debug.Log($"The pistol has {ammo} ammo!");
+            }
         }
-        else
+    }
+
+    void OnContinuousFire()
+    {
+        if (playerHasMinigun)
         {
-            GameObject bullet = Instantiate(PlayerBullet, PlayerGun.transform.position, transform.rotation);
-            //We create a bullet at the guns position and rotation as well as saving the information about that bullet in a gameobject variable.
-            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-            //We grab the rigidbodycomponent from the saved bullet and save it in the variable "rb".
-            rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
-            ammo -= 1;
-            //Adds force to the rb of the bullet and increases it acording to bulletSpeed. Also decreses ammo by one.
-            Debug.Log($"The pistol has {ammo} ammo!");
+            if (ammo < 1)
+            {
+                ResetAmmo();
+                Invoke("ReloadDone", reloadTime);
+                isReloading = true;
+                //If our ammo is 0 or less we start reloading, reset our ammo and makes sure we will stop reloading after reloadTime seconds.
+            }
+            if (isReloading)
+            {
+                return;
+            }
+            else
+            {
+                GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
+                //We create a bullet at the guns position and rotation as well as saving the information about that bullet in a gameobject variable.
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                //We grab the rigidbodycomponent from the saved bullet and save it in the variable "rb".
+                rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+                ammo -= 1;
+                //Adds force to the rb of the bullet and increases it acording to bulletSpeed. Also decreses ammo by one.
+                Debug.Log($"The minigun has {ammo} ammo!");
+            }
         }
     }
 
@@ -50,9 +128,30 @@ public class PlayerShooting : MonoBehaviour
         isReloading = false;
     }
 
+    private void ShotgunColdownDone()
+    {
+        shotgunColdown = false;
+    }
+
+    private void ResetAmmo()
+    {
+        if (playerHasPistol)
+        {
+            ammo = pistolAmmoMax;
+        }
+        else if (playerHasShotgun)
+        {
+            ammo = shotgunAmmoMax;
+        }
+        else if (playerHasMinigun)
+        {
+            ammo = minigunAmmoMax;
+        }
+    }
+
     void OnReload()
     {
-        ammo = ammoMax;
+        ResetAmmo();
         Invoke("ReloadDone", reloadTime);
         isReloading = true;
     }
