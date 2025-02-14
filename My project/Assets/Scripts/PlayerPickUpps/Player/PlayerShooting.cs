2b1a0f5a6daf3e08530f898f8278a39e4d11b61b
NilasAlbinson <nilas.albinson@gmail.com>
2025-02-14 15:48:21 +0100

    rdtfgyuhij

--- My project/Assets/Scripts/PlayerPickUpps/Player/PlayerShooting.cs
+++ My project/Assets/Scripts/PlayerPickUpps/Player/PlayerShooting.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using UnityEditor.UIElements;
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
