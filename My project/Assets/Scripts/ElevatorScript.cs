aa2df7ab5be9d8175a2b0fa3e98c29c56c24bb58
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-02-16 18:19:42 +0100

    sss

--- My project/Assets/Scripts/ElevatorScript.cs
+++ My project/Assets/Scripts/ElevatorScript.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ElevatorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            
+            Invoke("SwitchScene", 3f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SwitchScene()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
