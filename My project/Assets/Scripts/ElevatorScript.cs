bd6a3fbd2a095b43b5e27e5d0db6e190b7ab9048
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-02-16 21:01:10 +0100

    dadadada

--- My project/Assets/Scripts/ElevatorScript.cs
+++ My project/Assets/Scripts/ElevatorScript.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class ElevatorScript : MonoBehaviour
 {
+    Animator ani;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            
+            ani.SetBool("GotEntered", true);
             Invoke("SwitchScene", 3f);
         }
     }
